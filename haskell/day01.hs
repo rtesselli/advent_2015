493c94cfc91f6f6557971eba64cedaa1e528aa97
Riccardo Tesselli <riccardo.tesselli@gmail.com>
2024-06-19 18:21:40 +0200

    Add day01 Haskell

--- haskell/day01.hs
+++ haskell/day01.hs
@@ -1,4 +1,8 @@
+countParentheses :: String -> Int
+countParentheses [] = 0
+countParentheses (x:xs) = if x == '(' then 1 + countParentheses xs else -1 + countParentheses xs
+
 main :: IO ()
 main = do
   contents <- readFile "../data/day01.txt"
-  putStrLn contents
+  print(countParentheses contents)
