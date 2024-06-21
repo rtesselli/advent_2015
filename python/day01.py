30ede45ed589380bd7e06f9270a7f3611cc920dd
Riccardo Tesselli <riccardo.tesselli@gmail.com>
2024-06-21 17:43:22 +0200

    Add day01 Python part 2

--- python/day01.py
+++ python/day01.py
@@ -7,9 +7,19 @@ def count_parentheses(content: str) -> int:
     return counts['('] - counts[')']
 
 
+def find_basement(content: str) -> int | None:
+    floor = 0
+    for index, parentheses in enumerate(content):
+        if floor < 0:
+            return index
+        floor += 1 if parentheses == '(' else -1
+    return None
+
+
 def main():
     content = Path('../data/day01.txt').read_text()
     print(count_parentheses(content))
+    print(find_basement(content))
 
 
 if __name__ == '__main__':
