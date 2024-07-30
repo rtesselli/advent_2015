047f366364e511a58786b0305cf3ea4acd4c2e58
Riccardo Tesselli <riccardo.tesselli@gmail.com>
2024-07-30 17:28:28 +0200

    Add day03 Python part 2

--- python/day03.py
+++ python/day03.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+from more_itertools import chunked
 
 def read_moves():
     return Path('../data/day03.txt').read_text()
@@ -22,9 +22,39 @@ def visited_homes(moves: str) -> set[tuple[int, int]]:
     return visited
 
 
+def visited_homes2(moves: str) -> set[tuple[int, int]]:
+    x1, y1 = 0, 0
+    x2, y2 = 0, 0
+    visited = {(x1, y1)}
+    for first, second in chunked(moves, 2):
+        match first:
+            case '^':
+                y1 += 1
+            case 'v':
+                y1 -= 1
+            case '>':
+                x1 += 1
+            case '<':
+                x1 -= 1
+        visited.add((x1, y1))
+        match second:
+            case '^':
+                y2 += 1
+            case 'v':
+                y2 -= 1
+            case '>':
+                x2 += 1
+            case '<':
+                x2 -= 1
+        visited.add((x2, y2))
+    return visited
+
+
 def main():
-    coordinates = visited_homes(read_moves())
+    moves = read_moves()
+    coordinates = visited_homes(moves)
     print(len(coordinates))
+    print(len(visited_homes2(moves)))
 
 
 if __name__ == '__main__':
