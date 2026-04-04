850ec0ef46a72fbb1bfb8dc0a3f561adf055b6a6
Riccardo Tesselli <riccardo.tesselli@gmail.com>
2026-04-04 18:28:52 +0200

    Add day09 part 1

---
+++ python/day09.py
@@ -0,0 +1,31 @@
+import networkx as nx
+from pathlib import Path
+from itertools import permutations, pairwise
+from typing import Sequence
+
+
+def load_graph() -> nx.Graph:
+    lines = Path("data/day09.txt").read_text().splitlines()
+    g = nx.Graph()
+    for line in lines:
+        path, distance = line.split(" = ")
+        from_, to = path.split(" to ")
+        g.add_edge(from_, to, weight=int(distance))
+    return g
+
+
+def get_distance(g: nx.Graph, path: Sequence[str]) -> int:
+    distance = 0
+    for u, v in pairwise(path):
+        distance += g[u][v]["weight"]
+    return distance
+
+
+def get_distance_brute_force(g: nx.Graph) -> int:
+    return min(get_distance(g, path) for path in permutations(g.nodes))
+
+
+if __name__ == '__main__':
+    graph = load_graph()
+    print(graph.nodes)
+    print(get_distance_brute_force(graph))
