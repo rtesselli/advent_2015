2c1025f9b0ef1951cb86aaa88d2890d2642fb1eb
Riccardo Tesselli <riccardo.tesselli@gmail.com>
2026-04-07 18:43:50 +0200

    Add day12 part 2

--- python/day12.py
+++ python/day12.py
@@ -6,21 +6,23 @@ def load_data():
         return json.load(f)
 
 
-def find_numbers(payload: list | dict) -> int:
+def find_numbers(payload: list | dict, skip_red: bool = False) -> int:
     counter = 0
     if isinstance(payload, list):
         for obj in payload:
             if isinstance(obj, int):
                 counter += obj
             elif isinstance(obj, list) or isinstance(obj, dict):
-                counter += find_numbers(obj)
+                counter += find_numbers(obj, skip_red)
         return counter
     elif isinstance(payload, dict):
+        if skip_red and "red" in payload.values():
+            return 0
         for obj in payload.values():
             if isinstance(obj, int):
                 counter += obj
             elif isinstance(obj, list) or isinstance(obj, dict):
-                counter += find_numbers(obj)
+                counter += find_numbers(obj, skip_red)
         return counter
     return 0
 
@@ -28,3 +30,4 @@ def find_numbers(payload: list | dict) -> int:
 if __name__ == '__main__':
     data = load_data()
     print(find_numbers(data))
+    print(find_numbers(data, skip_red=True))
