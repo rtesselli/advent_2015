c497788547a4d2b8d9976102748ca3b1a771606f
Riccardo Tesselli <riccardo.tesselli@gmail.com>
2026-05-18 16:51:57 +0200

    Add day13 part 1

---
+++ python/day13.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+from pathlib import Path
+from itertools import permutations, pairwise
+
+
+def load_preferences() -> dict:
+    lines = Path("data/day13.txt").read_text().splitlines()
+    data = defaultdict(dict)
+    for line in lines:
+        terms = line.split()
+        subj = terms[0]
+        target = terms[-1][:-1]
+        sign = 1 if terms[2] == 'gain' else -1
+        amount = int(terms[3])
+        data[subj][target] = sign * amount
+    return data
+
+
+def get_best_happiness(preferences: dict) -> int:
+    attendees = list(preferences.keys())
+    first = attendees.pop(0)
+    best_score = 0
+    for permutation in permutations(attendees):
+        sequence = [first] + list(permutation)
+        score = 0
+        for a, b in pairwise(sequence):
+            score += preferences[a][b]
+            score += preferences[b][a]
+        score += preferences[sequence[0]][sequence[-1]]
+        score += preferences[sequence[-1]][sequence[0]]
+        best_score = max(best_score, score)
+    return best_score
+
+
+if __name__ == '__main__':
+    links = load_preferences()
+    print(get_best_happiness(links))
